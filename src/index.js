0bd212a508d1a88bab332299cad58422d439895d
PamPT <p.pachecotorrico@gmail.com>
2025-03-01 16:09:12 +0100

    Added search functionality using API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "ca301o346f2bf4fa483tcabaf1a295d2";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function searchSubmit(event) {
   event.preventDefault();
   let serachInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#search-city");
-  cityElement.innerHTML = serachInput.value;
+
+  searchCity(serachInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", searchSubmit);
+
+searchCity("Zagreb");
